86cc911cdfbf6d75a6ff66ed7a905ac458b10257
AhmidouA <ahmedakkioui75@gmail.com>
2023-12-23 16:42:43 +0100

    Adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Navbar from '@/scenes/navbar';
 import Home from '@/scenes/home';
-import Benefits from '@/scenes/benefits';
+import Benefits from '@/scenes/Benefits';
 import OurClasses from '@/scenes/ourClasses';
 import ContactUs from '@/scenes/contactUs';
 import Footer from '@/scenes/footer';
