3c13e71d626b69df4a2e06b8d0aa27afc68fbdea
AhmidouA <ahmedakkioui75@gmail.com>
2023-12-23 14:35:04 +0100

    OurClasses Section HomePage Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/scenes/navbar';
 import Home from '@/scenes/home';
-import Benefits from './scenes/Benefits';
+import Benefits from '@/scenes/benefits';
+import OurClasses from '@/scenes/ourClasses';
 import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
 
@@ -36,6 +37,7 @@ function App() {
 
       <Home setSelectedPage={setSelectedPage}/>
       <Benefits setSelectedPage={setSelectedPage}/>
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
     
   )
